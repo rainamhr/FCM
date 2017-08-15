d479963554450861d395888e9d8e68b04e47dba1
rainamhr <rainamhr627@gmail.com>
2017-08-15 18:46:00 +0545

    aaaaaa

--- app/src/main/java/com/example/queen/fcm/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/queen/fcm/service/MyFirebaseMessagingService.java
@@ -2,10 +2,13 @@ package com.example.queen.fcm.service;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -17,7 +20,6 @@ import android.util.Log;
 import com.example.queen.fcm.R;
 import com.example.queen.fcm.activity.MainActivity;
 import com.example.queen.fcm.app.Config;
-import com.example.queen.fcm.utils.NotificationUtils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -37,8 +39,8 @@ import static com.example.queen.fcm.R.attr.title;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    private Context mContext;
     private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
-    private NotificationUtils notificationUtils;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -74,8 +76,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
 
         // play notification sound
-        NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
-        notificationUtils.playNotificationSound();
+        playNotificationSound();
+
     }
 
     private void handleDataMessage(JSONObject json) {
@@ -84,40 +86,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         try {
 
             JSONObject data = json.getJSONObject("data");
-
-            String title = data.getString("title");
-            String message = data.getString("message");
-            boolean isBackground = data.getBoolean("is_background");
-            String type = data.getString("type");
-            String timestamp = data.getString("timestamp");
             JSONObject payload = data.getJSONObject("payload");
             String payload_type = payload.get("type").toString();
             String payloadData = payload.get("data").toString();
-//            String videoUrl = (String) payload.get("url");
-//            String camera = (String) payload.get("pic");
-//            //String callLog = (String) payload.get("callLog");
-//            String playStore = (String) payload.get("url1");
-
-//            Log.e(TAG, "title: " + title);
-//            Log.e(TAG, "message: " + message);
-//            Log.e(TAG, "isBackground: " + isBackground);
-//            Log.e(TAG, "payload: " + payload.get("type"));
-//            Log.e(TAG, "type: " + type);
-//            Log.e(TAG, "timestamp: " + timestamp);
-//            Log.e(TAG, "url" + videoUrl);
-//            Log.e(TAG,"camera:" +camera);
-//            //Log.e(TAG,"callLog:" +callLog);
-//            Log.e(TAG,"url1:" +playStore);
-
 
             SharedPreferences.Editor saveData = getSharedPreferences("datapayload", MODE_PRIVATE).edit();
 
             saveData.putString("type", payload_type);
             saveData.putString("payload data", payloadData);
-//            saveData.putString("videoURL", videoUrl);
-//            saveData.putString("camera", camera);
-            //saveData.putString("callLog", callLog);
-//            saveData.putString("playStore", playStore);
 
             saveData.commit();
             saveData.clear();
@@ -140,19 +116,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Intent intent = new Intent();
 
             if (type.equalsIgnoreCase("video")) {
-                try{
-                    if (getPackageManager().getPackageInfo(payloadData, PackageManager.GET_ACTIVITIES) != null){
-                        intent = getPackageManager()
-                                .getLaunchIntentForPackage("com.check.application");
-                    }
-                    else{
-                        intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + payloadData));
-                    }
-                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse(payloadData));
-                }
-                catch (Exception e){
-
-                }
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(payloadData));
             }
             else if (type.equalsIgnoreCase("camera")) {
                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -160,8 +124,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 intent.putExtra(MediaStore.EXTRA_OUTPUT,
                         Uri.fromFile(photo));
             }
-            else if (type.equalsIgnoreCase("playStore")) {/*
-                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object*/
+            else if (type.equalsIgnoreCase("playStore")) {
                 try {
                     intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + payloadData));
                 } catch (android.content.ActivityNotFoundException anfe) {
@@ -187,10 +150,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             }
 
             Log.d("notification", data.getString("title"));
-//            Intent intent = new Intent(this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            // intent.putExtra("type",data.getString("type"));
-            // intent.putExtra("type",data.getString("videoURL"));
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
             NotificationCompat.Builder notficationBuilder = new NotificationCompat.Builder(this);
             notficationBuilder.setContentTitle(data.getString("title"));
@@ -201,6 +161,19 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.notify(0, notficationBuilder.build());
         } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public void playNotificationSound() {
+
+        Log.d("notification sound", "entered");
+
+        try {
+            Uri alarmSound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + "://" + mContext.getPackageName() + "/raw/notification");
+            Ringtone r = RingtoneManager.getRingtone(mContext, alarmSound);
+            r.play();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
